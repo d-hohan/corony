73200b3779fd6af053c015f62386847c618de64a
Arinc Firincioglu <arinc.firincioglu@smail.th-koeln.de>
2021-06-07 20:34:35 +0200

    init commit

---
+++ back-end/corony/src/test/java/com/arenchf/corony/CoronyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arenchf.corony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
